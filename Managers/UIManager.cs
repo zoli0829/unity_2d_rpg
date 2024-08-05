ba98862515f8e9f3f43065a960872f44886ed625
ZoltanV <zoli0829@gmail.com>
2024-08-05 15:46:05 +0100

    started the environment

---
+++ Managers/UIManager.cs
@@ -0,0 +1,38 @@
+
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField] private PlayerStats stats;
+
+    [Header("Bars")]
+    [SerializeField] private Image healthBar;
+    [SerializeField] private Image manaBar;
+    [SerializeField] private Image expBar;
+
+    [Header("Text")]
+    [SerializeField] private TextMeshProUGUI levelTMP;
+    [SerializeField] private TextMeshProUGUI healthTMP;
+    [SerializeField] private TextMeshProUGUI manaTMP;
+    [SerializeField] private TextMeshProUGUI expTMP;
+
+    private void Update()
+    {
+        UpdatePlayerUI();   
+    }
+
+    private void UpdatePlayerUI()
+    {
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, stats.Health / stats.MaxHealth, 10f * Time.deltaTime);
+        manaBar.fillAmount = Mathf.Lerp(manaBar.fillAmount, stats.Mana / stats.MaxMana, 10f * Time.deltaTime);
+        expBar.fillAmount = Mathf.Lerp(expBar.fillAmount, stats.CurrentExperience / stats.NextLevelExperience, 10f * Time.deltaTime);
+
+        levelTMP.text = $"Level {stats.Level}";
+        healthTMP.text = $"{stats.Health} / { stats.MaxHealth}";
+        manaTMP.text = $"{stats.Mana} / {stats.MaxMana}";
+        expTMP.text = $"{stats.CurrentExperience} / {stats.NextLevelExperience}";
+    }
+}
